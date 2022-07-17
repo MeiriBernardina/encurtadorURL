026f3d1ba33239230c8a9aed9fa0a7e120415d87
MeiriBernardina <meiri.umc@gmail.com>
2022-07-17 18:56:09 -0300

    final

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://meirib:123456@urlshortenermeiri.tkqij6m.mongodb.net/urlshortenermeiri?retryWrites=true&w=majority',
+}
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { URLController } from 'controler/URLController'
+import { MongoConnection } from './database/MongoConnection'
+import express from 'express'
+const api = express()
+api.use(express.json())
+
+const database = new MongoConnection()
+database.connect()
+
+const urlController = new URLController()
+api.post('/shorten', urlController.shorten)
+api.get('/:hash', urlController.redirect)
+
+//api.get('/test', (req: Request, res:Response) => {
+   // res.json({success: true })
+//})
+api.listen(5000, () => console.log('Express listening'))
